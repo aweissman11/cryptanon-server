d30d1b31a10f69d6f05ceb14b2c487dc79930ec0
Graham <g@masterofnone.com>
2018-12-04 16:48:36 -0700

    Add  asset and specific asset routes

--- server.js
+++ server.js
@@ -13,6 +13,19 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'CryptAnon';
 app.locals.assets = assets;
 
+
+// 1 GET endpoints for all of one resource (i.e. ‘/api/v1/merchants’)
+// 1 GET endpoints for a specific resource (i.e. ‘/api/v1/merchants/:id’)
+// 2 POST endpoints
+  // post user
+  // add user favorite
+// 2 PUT or PATCH endpoints
+  // PUT || PATCH username 
+  // PUT || PATCH password
+// 2 DELETE endpoints
+  // delete user
+  // delete favorite
+
 app.get('/', (request, response) => {
   response.send('This is the home route. HTML, JS, and  CSS go here');
 });
@@ -27,29 +40,49 @@ app.get('/api/v1/assets', (request, response) => {
     })
 })
 
-app.post('/api/v1/assets', (request, response) => {
-  const asset = request.body;
+app.get('/api/v1/assets/:asset_ID/asset_prices', (request, response) => {
+  const { asset_ID } = request.params;
 
-  let missingProperties = [];
-
-  for (let requiredProperty of ['name', 'ticker', 'type', 'icon_url', 'website_url']) {
-    if(asset[requiredProperty] === undefined) {
-      missingProperties = [...missingProperties, requiredProperty]
-    }
-  }
-
-  if (missingProperties.length) {
-    response
-      .status(422)
-      .send({ error: `missing required param/s: ${missingProperties}`})
-  }
-
-
-  app.locals.assets = [...app.locals.assets, asset];
-  response.status(201).json(asset)
-  // sometimes better to just send back a message
-  // response.status(201).json({ message: `${student.name} added to DB`})
+  database('asset_prices').where('asset_id', asset_ID).select()
+    .then(prices => {
+      if (prices.length) {
+        response.status(200).json(prices);
+      } else {
+        response.status(404).json( { 
+          error: `could not find any asset prices for asset id: ${asset_ID}`
+        })
+      }
+    })
+    .catch(error => {
+      response.status(500).json( { error } )
+    })
 })
+
+
+
+// app.post('/api/v1/assets', (request, response) => {
+//   const asset = request.body;
+
+//   let missingProperties = [];
+
+//   for (let requiredProperty of ['name', 'ticker', 'type', 'icon_url', 'website_url']) {
+//     if(asset[requiredProperty] === undefined) {
+//       missingProperties = [...missingProperties, requiredProperty]
+//     }
+//   }
+
+//   if (missingProperties.length) {
+//     response
+//       .status(422)
+//       .send({ error: `missing required param/s: ${missingProperties}`})
+//   }
+
+
+//   app.locals.assets = [...app.locals.assets, asset];
+//   response.status(201).json(asset)
+//   // sometimes better to just send back a message
+//   // response.status(201).json({ message: `${student.name} added to DB`})
+// })
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on localhost:${app.get('port')}.`);
