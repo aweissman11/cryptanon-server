abec950d84bfd42e0992e5737305653b229dc09c
Aaron Weissman <weissman.aaron@gmail.com>
2018-12-13 11:54:27 -0700

    unignore build

---
+++ client/front-end-cryptanon/build/precache-manifest.a0f51a1af7ceefb85a9333367c0b720a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c213abd5ae58fde1755",
+    "url": "/static/css/main.412935ab.chunk.css"
+  },
+  {
+    "revision": "6c213abd5ae58fde1755",
+    "url": "/static/js/main.6c213abd.chunk.js"
+  },
+  {
+    "revision": "a4c27249dd136d47d0f4",
+    "url": "/static/js/1.a4c27249.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8901b9c789792f4f7af0e3bdb5476403",
+    "url": "/index.html"
+  }
+];
